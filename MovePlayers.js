cb64da225a2780e355be1aea9d7ab7c8be424350
Rivky Weingarten <RW@HS.local>
2023-06-14 18:04:23 -0400

    need to fix issues

--- MovePlayers.js
+++ MovePlayers.js
@@ -1,11 +1,21 @@
-console.log("hi from move");
-const scoreBoard = document.getElementById("header5");
+
+const scoreBoard = document.querySelector(".header5");
+const header = document.querySelector("h1");
 const player1Score = document.getElementById("score-player1");
 const player2Score = document.getElementById("score-player2");
+//what does this code do? user clicks button to begin
+//then program decides which player's turn it is in function called switchPlayer
+//it then calls function throwdice and passes in char (red or blue) into function
+//throw dice first randomizes number from 1 to 6, it calls function called DicePicture in dice.js
+//which decides which picture to display based on random number
+//it then defines destination cell which is starting cell plus the dice number
+//it moves the charachter one div at a time to the destination cell in function move
+//it also checks the destination number if it is a snake or a ladder then calls snakes snakesLadder.js which specifies the path of the snake or the ladder.
+//until one charachter wins.
+
 
 let red = {
   name: "player1",
-
   startingCell: 0,
   destinationCell: 0,
   playerNum: 1,
@@ -13,13 +23,20 @@ let red = {
 
 let blue = {
   name: "player2",
-
   startingCell: 0,
   destinationCell: 0,
   playerNum: 2,
 };
 
 let playerCh = player1;
+
+
+const dice1 = document.getElementById("dice");
+console.log(dice1);
+dice1.addEventListener("click", (e) => {
+  e.preventDefault();
+  switchPlayer();
+});
 
 function switchPlayer() {
   if (playerCh === "player1") {
@@ -46,12 +63,7 @@ function move(nextCellNum, playerCh) {
   playerScore.innerText = nextCellNum;
 }
 
-const dice1 = document.getElementById("dice");
-console.log(dice1);
-dice1.addEventListener("click", (e) => {
-  e.preventDefault();
-  switchPlayer();
-});
+
 
 function throwdice(player, playerCh) {
   let diceNum = Math.floor(Math.random() * 6) + 1;
@@ -59,6 +71,24 @@ function throwdice(player, playerCh) {
   player.destinationCell = player.startingCell + diceNum;
 
   // player.destinationCell = newDestination
+  
+
+ 
+  player.score = player.destinationCell;
+
+  const positionsToMove = [];
+  for (let k = player.startingCell + 1; k <= player.destinationCell; k++) {
+    positionsToMove.push(k);
+  }
+  let delay = 0;
+  positionsToMove.forEach((position) => {
+    setTimeout(() => {
+      move(position, playerCh);
+    }, delay);
+    delay += 300;
+  });
+
+  player.startingCell = player.destinationCell;
   if (player.destinationCell === 2) {
     ladderSnake(playerCh, cell2);
     player.startingCell = 38;
@@ -102,26 +132,12 @@ function throwdice(player, playerCh) {
   if (player.destinationCell > 100) {
     return;
   }
-
   if (player.destinationCell === 100) {
     
     snakesBoard.style.display = "none";
     scoreBoard.style.display = "none";
+    header.innerText="WHOO HOO" + player.name +'WON!'
+    
 
       }
-  player.score = player.destinationCell;
-
-  const positionsToMove = [];
-  for (let k = player.startingCell + 1; k <= player.destinationCell; k++) {
-    positionsToMove.push(k);
-  }
-  let delay = 0;
-  positionsToMove.forEach((position) => {
-    setTimeout(() => {
-      move(position, playerCh);
-    }, delay);
-    delay += 300;
-  });
-
-  player.startingCell = player.destinationCell;
 }
