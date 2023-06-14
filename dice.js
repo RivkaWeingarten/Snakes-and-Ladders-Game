2c7a335217a63cce014262a2c482812054c352e0
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-06-13 23:21:39 -0400

    a couple of styling changes

--- dice.js
+++ dice.js
@@ -5,8 +5,8 @@ dicePic=document.createElement('img')
 function DicePicture(diceNum){
  dicePic.src='assets/dice'+ diceNum + '.png'
  dicePic.alt='picture of dice'+ diceNum
- dicePic.style.height='125px';
- dicePic.style.width='125px';
+ dicePic.style.height='75px';
+ dicePic.style.width='75px';
  dice.innerHTML="";
  dice.appendChild(dicePic)
 }
